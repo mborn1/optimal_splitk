9970821e56a06bdd4f3f771d4a0623ef786f8f84
Mathias Born <44361767+mborn1@users.noreply.github.com>
2024-02-16 13:41:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,9 @@ sys.path.append(
     str(pathlib.Path(__file__).parents[2].resolve() / 'src')
 )
 import datetime
-from optimal_splitk import __version__ as lib_version
+
+with open('src/optimal_splitk/__init__.py') as f:
+    lib_version = re.search(r'__version__ = [\'"](.*)[\'"]', f.read()).group(1)
 
 
 # -- Project information -----------------------------------------------------
